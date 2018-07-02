018303d51470180665f189d0ae46b96901c6c93f
Jexan <Jexan13@gmail.com>
2018-07-01 22:43:42 -0400

    Started the implementation of the Column abstract class and the Text class.

--- OxygenRM/internals/columns.py
+++ OxygenRM/internals/columns.py
@@ -1,24 +1,71 @@
-class Column():
-    def __init__(self):
-        pass
+from abc import *
 
-class Id(Column):
-    def __init__(self):
-        pass
+class Column(metaclass=ABCMeta):
+    ''' The base class for defining a Model (or Migration)
+        column.
+    '''
+    def __init__(self, **options):
+        self.default = options.get('default', None)
+        self.null    = options.get('null', False)
+        self.primary = options.get('primary', False)
+        self.auto_increment = options.get('auto_increment', False)
 
-class String(Column):
-    def __init__(self):
-        pass
+        self._value = self.default
+
+    def __set__(self, instance, value):
+        ''' Validate the set value and change the internal 
+            _value of the class 
+        '''
+        if not isinstance(value, self.value_class):
+            raise TypeError('Property value cannot be of type: {}. \'{}\' expected'.format(
+                type(value), self.value_class.__name__))
+
+        if self.validate(value):
+            self._value = value
+
+    def __get__(self, instance, owner):
+        ''' Get the value of the column
+
+            Returns:
+                The column internal value.
+        '''
+        return self._value
+
+    def validate(self, value):
+        ''' Decide wheter a value that wants to be set is valid.
+
+            Args:
+                value: The value to be set internally
+        
+            Returns:
+                A bool.
+        '''
+        return True
+
+    ''' The value of the column itself
+    '''
+    _value = None
+
+    ''' The class of the value.
+    '''
+    value_class = None
+
+class Text(Column):
+    ''' A basic Text column.
+    '''
+
+    ''' A dict with the types this column represents
+        in various database systems.
+    '''
+    sql_type = {'sqlite3': 'text'}
+
+    value_class = str
 
 class Bool(Column):
     pass
 
 class Integer(Column):
     pass
-
-class Text(Column):
-    def __init__(self):
-        pass
 
 class Rel(Column):
     def __init__(self):
@@ -37,3 +84,14 @@ class Date(Column):
     
 class Float(Column):
     pass
+
+class Id(Column):
+    ''' An auto-incrementing, unsigned integer. Used as a primary key.
+    '''
+
+    null = False
+    primary = True
+    auto_increment = True
+
+    def __init__(self, name='id'):
+        self.name = name
